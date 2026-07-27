bb16894423ca2157ab1a1ccf8e69fb2423511581
VRA <ambionvra21@gmail.com>
2026-07-27 22:50:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // ==========================================
-// 🚀 Infinity Alpha Boot Loader
+// INFINITY ALPHA OS
+// Boot Loader
 // ==========================================
 
-console.log("🚀 Booting Infinity Alpha OS...");
+console.log("🚀 Booting Infinity Alpha OS");
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    console.log("✅ Infinity Alpha OS Ready");
+
+});
